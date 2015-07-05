813e1b00dd4eb48dff03a8ebe4620a1f064fad6c
Felipe Nesello <felipenesello@gmail.com>
2015-07-05 14:45:26 -0300

    GreenNetworkController through full path routing.

---
+++ src/main/java/net/floodlightcontroller/greennetwork/GNCPacketInProcessor.java
@@ -0,0 +1,301 @@
+package net.floodlightcontroller.greennetwork;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.util.AppCookie;
+import net.floodlightcontroller.devicemanager.IDevice;
+import net.floodlightcontroller.devicemanager.IDeviceService;
+import net.floodlightcontroller.devicemanager.SwitchPort;
+import net.floodlightcontroller.forwarding.Forwarding;
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.routing.Route;
+import net.floodlightcontroller.topology.NodePortTuple;
+import net.floodlightcontroller.util.MatchUtils;
+import net.floodlightcontroller.util.OFMessageDamper;
+
+import org.projectfloodlight.openflow.protocol.OFFactory;
+import org.projectfloodlight.openflow.protocol.OFFlowMod;
+import org.projectfloodlight.openflow.protocol.OFPacketIn;
+import org.projectfloodlight.openflow.protocol.OFPacketOut;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.OFVersion;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.action.OFActionOutput;
+import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.types.DatapathId;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.OFBufferId;
+import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.U64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple packet processor, based on {@link Forwarding}.
+ * @author felipe.nesello
+ *
+ */
+public class GNCPacketInProcessor {
+
+	private static final int GNC_APP_ID = 16;
+
+	private static final int FLOWMOD_DEFAULT_IDLE_TIMEOUT = 10;
+	private static final int FLOWMOD_DEFAULT_HARD_TIMEOUT = 10;
+	private static final int FLOWMOD_DEFAULT_PRIORITY = 1;
+
+	private static int OFMESSAGE_DAMPER_CAPACITY = 10000;
+	private static int OFMESSAGE_DAMPER_TIMEOUT = 250; // ms
+
+	protected static Logger logger = LoggerFactory.getLogger(GNCPacketInProcessor.class);
+
+	private final OFMessageDamper messageDamper;
+	private final GreenNetworkController controller;
+
+	static {
+		AppCookie.registerApp(GNC_APP_ID, "GreenNetwork");
+	}
+	public static final U64 appCookie = AppCookie.makeCookie(GNC_APP_ID, 0);
+
+	public GNCPacketInProcessor(GreenNetworkController controller) {
+		this.controller = controller;
+		this.messageDamper = new OFMessageDamper(OFMESSAGE_DAMPER_CAPACITY, EnumSet.of(OFType.FLOW_MOD), OFMESSAGE_DAMPER_TIMEOUT);
+	}
+
+	public boolean processPacket(IOFSwitch sw, FloodlightContext cntx, OFPacketIn packetIn) {
+		Ethernet ethernetFrame = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+		OFPort ingressPort = (packetIn.getVersion().compareTo(OFVersion.OF_12) < 0 ?
+				packetIn.getInPort() : packetIn.getMatch().get(MatchField.IN_PORT));
+		boolean hasProcessCompletedSuccessfully = false;
+
+		if (ethernetFrame.isBroadcast()) {
+			// If traffic is broadcast (ARP), flood to all ports
+			hasProcessCompletedSuccessfully = processAsBroadcast(sw, packetIn, ingressPort);
+		} else {
+			// Else process packet as unicast
+			hasProcessCompletedSuccessfully = processAsUnicast(sw, packetIn, ingressPort, cntx);
+		}
+
+		return hasProcessCompletedSuccessfully;
+	}
+
+	private boolean processAsUnicast(IOFSwitch sw, OFPacketIn packetIn, OFPort ingressPort, FloodlightContext cntx) {
+		// Check if we the destination is reachable
+		IDevice dstDevice = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_DST_DEVICE);
+
+		if (dstDevice != null) {
+			IDevice srcDevice = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_SRC_DEVICE);
+
+			if (srcDevice != null) {
+				if (!isIngressPortSameAsDestinationPort(sw, dstDevice, ingressPort)) {
+					return addFlowsOnPathBetween(srcDevice, dstDevice, sw, ingressPort, cntx, packetIn);
+				} else {
+					logger.error("Both source and destination are on the same switch/port {}/{}, Action = NOP",
+							sw.getId().toString(), ingressPort);
+					return false;
+				}
+			} else {
+				logger.error("No device entry found for source device");
+				return false;
+			}
+		} else {
+			// Treat as broadcast, because the destination was not found
+			logger.info("Destination not found, flooding packet {}...", packetIn);
+			return processAsBroadcast(sw, packetIn, ingressPort);
+		}
+	}
+
+	private boolean addFlowsOnPathBetween(IDevice srcDevice, IDevice dstDevice, IOFSwitch sw, OFPort ingressPort,
+			FloodlightContext cntx, OFPacketIn packetIn) {
+		// assuming that every switch is connected to only one host
+		// FIXME improve method to get 'N' hosts
+		SwitchPort srcSwitchPort = srcDevice.getAttachmentPoints()[0];
+		SwitchPort dstSwitchPort = dstDevice.getAttachmentPoints()[0];
+
+		if (!srcSwitchPort.equals(dstSwitchPort)) {
+			// TODO propose a new routing service that routes through a energy saving path
+			Route route = controller.routingService.getRoute(
+					srcSwitchPort.getSwitchDPID(), srcSwitchPort.getPort(),
+					dstSwitchPort.getSwitchDPID(), dstSwitchPort.getPort(),
+					U64.of(0));
+
+			if (route != null) {
+				Match match = createMatchFromPacket(sw, ingressPort, cntx);
+				return pushRouteFlowOnAllHops(route, match, packetIn, sw.getId(), cntx, ingressPort);
+			}
+		}
+		return false;
+	}
+
+	private boolean pushRouteFlowOnAllHops(Route route, Match match, OFPacketIn packetIn,
+			DatapathId packetInSwitch, FloodlightContext cntx, OFPort ingressPort) {
+		List<NodePortTuple> switchPortList = route.getPath();
+		boolean hasPushedAllHops = true;
+
+		// FIXME improve this iteration to iterate on switches, and not depending on indexes
+		for (int indx = switchPortList.size() - 1; indx > 0; indx -= 2) {
+			DatapathId switchDPID = switchPortList.get(indx).getNodeId();
+			IOFSwitch sw = controller.switchService.getSwitch(switchDPID);
+
+			if (sw != null) {
+				OFFactory factory = sw.getOFFactory();
+
+				OFFlowMod.Builder flowBuilder = factory.buildFlowAdd();
+
+				OFActionOutput.Builder actionOutputBuilder = factory.actions().buildOutput();
+				List<OFAction> actions = new ArrayList<OFAction>();	
+				Match.Builder matchBuilder = MatchUtils.createRetentiveBuilder(match);
+
+				// set input and output ports on the switch
+				OFPort outPort = switchPortList.get(indx).getPortId();
+				OFPort inPort = switchPortList.get(indx - 1).getPortId();
+
+				matchBuilder.setExact(MatchField.IN_PORT, inPort);
+				actionOutputBuilder.setPort(outPort);
+				actionOutputBuilder.setMaxLen(Integer.MAX_VALUE);
+				actions.add(actionOutputBuilder.build());
+
+				flowBuilder.setMatch(matchBuilder.build())
+				.setActions(actions)
+				.setIdleTimeout(FLOWMOD_DEFAULT_IDLE_TIMEOUT)
+				.setHardTimeout(FLOWMOD_DEFAULT_HARD_TIMEOUT)
+				.setBufferId(OFBufferId.NO_BUFFER)
+				.setCookie(appCookie)
+				.setOutPort(outPort)
+				.setPriority(FLOWMOD_DEFAULT_PRIORITY);
+
+				try {
+					logger.info("Pushing route flowmod routeIndx={} sw={} inPort={} outPort={}",
+							indx, sw, flowBuilder.getMatch().get(MatchField.IN_PORT), outPort);
+					
+					hasPushedAllHops &= messageDamper.write(sw, flowBuilder.build());
+
+					if (isSourceSwitch(packetInSwitch, sw)) {
+						// if it is the source switch, must forward the packet in
+						// to the same output port of flow added
+						sendPacketOut(sw, packetIn, outPort, cntx, ingressPort);
+					}
+				} catch (IOException e) {
+					logger.error("Failure writing flow mod", e);
+					hasPushedAllHops = false;
+				}
+			} else {
+				logger.error("Switch with DPID {} not found", switchDPID.toString());
+				hasPushedAllHops = false;
+			}
+		}
+
+		return hasPushedAllHops;
+	}
+
+	private boolean isSourceSwitch(DatapathId packetInSwitch, IOFSwitch sw) {
+		return sw.getId().equals(packetInSwitch);
+	}
+
+	private void sendPacketOut(IOFSwitch sw, OFPacketIn packetIn,
+			OFPort outPort, FloodlightContext cntx, OFPort ingressPort) {
+		if (!ingressPort.equals(outPort)) {
+			// Send a packet out
+			OFFactory factory = sw.getOFFactory();
+			OFPacketOut.Builder packetOutbuilder = factory.buildPacketOut();
+
+			List<OFAction> actions = new ArrayList<OFAction>();
+			actions.add(factory.actions().output(outPort, Integer.MAX_VALUE));
+
+			packetOutbuilder.setActions(actions);
+			packetOutbuilder.setBufferId(OFBufferId.NO_BUFFER);
+			packetOutbuilder.setData(packetIn.getData());
+			packetOutbuilder.setInPort(ingressPort);
+
+			try {
+				messageDamper.write(sw, packetOutbuilder.build());
+			} catch (IOException e) {
+				logger.error("Failure writing packet out", e);
+			}
+		}
+	}
+
+	private Match createMatchFromPacket(IOFSwitch sw, OFPort ingressPort, FloodlightContext cntx) {
+		Ethernet ethernetFrame = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+		MacAddress srcMacAddress = ethernetFrame.getSourceMACAddress();
+		MacAddress dstMacAddress = ethernetFrame.getDestinationMACAddress();
+
+		Match.Builder matchBuilder = sw.getOFFactory().buildMatch();
+		matchBuilder.setExact(MatchField.IN_PORT, ingressPort)
+		.setExact(MatchField.ETH_SRC, srcMacAddress)
+		.setExact(MatchField.ETH_DST, dstMacAddress);
+
+		if (ethernetFrame.getEtherType() == EthType.IPv4) {
+			IPv4 ipPacket = (IPv4) ethernetFrame.getPayload();
+			IPv4Address srcIpAddress = ipPacket.getSourceAddress();
+			IPv4Address dstIpAddress = ipPacket.getDestinationAddress();
+
+			matchBuilder.setExact(MatchField.ETH_TYPE, EthType.IPv4)
+			.setExact(MatchField.IPV4_SRC, srcIpAddress)
+			.setExact(MatchField.IPV4_DST, dstIpAddress);
+		} else if (ethernetFrame.getEtherType() == EthType.ARP) {
+			matchBuilder.setExact(MatchField.ETH_TYPE, EthType.ARP);
+		}
+
+		return matchBuilder.build();
+	}
+
+	private boolean isIngressPortSameAsDestinationPort(IOFSwitch sw, IDevice dstDevice, OFPort ingressPort) {
+		// Validate that the source and destination are not on the same switchport
+		boolean isTheSamePort = false;
+
+		for (SwitchPort dstAttachPoints : dstDevice.getAttachmentPoints()) {
+			DatapathId dstSwitchDpid = dstAttachPoints.getSwitchDPID();
+
+			if (isSourceSwitch(dstSwitchDpid, sw) && ingressPort.equals(dstAttachPoints.getPort())) {
+				isTheSamePort = true;
+			}
+			break;
+		}
+		return isTheSamePort;
+	}
+
+	private boolean processAsBroadcast(IOFSwitch sw, OFPacketIn packetIn, OFPort ingressPort) {
+		if (!controller.topologyService.isIncomingBroadcastAllowed(sw.getId(), ingressPort)) {
+			logger.error("Broadcast not allowed on port {} of switch {}", ingressPort.toString(), sw.getId().toString());
+			return false;
+		}
+
+		// Set Action to flood
+		OFFactory factory = sw.getOFFactory();
+		OFPacketOut.Builder packetOutBuilder = factory.buildPacketOut();
+		List<OFAction> actions = new ArrayList<OFAction>();
+
+		// If FLOOD port is supported, forward to flood instead of ALL
+		if (sw.hasAttribute(IOFSwitch.PROP_SUPPORTS_OFPP_FLOOD)) {
+			actions.add(factory.actions().output(OFPort.FLOOD, Integer.MAX_VALUE));
+		} else {
+			actions.add(factory.actions().output(OFPort.ALL, Integer.MAX_VALUE));
+		}
+		packetOutBuilder.setActions(actions);
+
+		// set buffer-id, in-port and packet-data based on packet-in
+		packetOutBuilder.setBufferId(OFBufferId.NO_BUFFER);
+		packetOutBuilder.setInPort(ingressPort);
+		packetOutBuilder.setData(packetIn.getData());
+
+		try {
+			logger.info("Writing flood PacketOut switch={} packet-in={} packet-out={}", sw.getId().toString(), packetIn, packetOutBuilder.build());
+			return messageDamper.write(sw, packetOutBuilder.build());
+		} catch (IOException e) {
+			logger.error("Failure writing PacketOut switch={} packet-in={} packet-out={}", sw.getId().toString(), packetIn, packetOutBuilder.build(), e);
+		}
+
+		return false;
+	}
+
+}
--- src/main/java/net/floodlightcontroller/greennetwork/GreenNetworkController.java
+++ src/main/java/net/floodlightcontroller/greennetwork/GreenNetworkController.java
@@ -2,9 +2,7 @@ package net.floodlightcontroller.greennetwork;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,54 +11,21 @@ import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
-import net.floodlightcontroller.core.IOFSwitchListener;
-import net.floodlightcontroller.core.PortChangeType;
-import net.floodlightcontroller.core.internal.IOFSwitchManager;
 import net.floodlightcontroller.core.internal.IOFSwitchService;
-import net.floodlightcontroller.core.internal.OFSwitchManager;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
-import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryListener;
-import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
-import net.floodlightcontroller.linkdiscovery.LinkInfo;
-import net.floodlightcontroller.linkdiscovery.internal.LinkDiscoveryManager;
-import net.floodlightcontroller.packet.Ethernet;
-import net.floodlightcontroller.packet.ICMP;
-import net.floodlightcontroller.packet.IPv4;
-import net.floodlightcontroller.routing.Link;
+import net.floodlightcontroller.devicemanager.IDeviceService;
+import net.floodlightcontroller.linkdiscovery.ILinkDiscovery.LDUpdate;
+import net.floodlightcontroller.routing.IRoutingService;
 import net.floodlightcontroller.topology.ITopologyListener;
 import net.floodlightcontroller.topology.ITopologyService;
-import net.floodlightcontroller.topology.TopologyInstance;
-import net.floodlightcontroller.topology.TopologyManager;
 
-import org.projectfloodlight.openflow.protocol.OFFactory;
-import org.projectfloodlight.openflow.protocol.OFFlowAdd;
-import org.projectfloodlight.openflow.protocol.OFMatchV3;
 import org.projectfloodlight.openflow.protocol.OFMessage;
-import org.projectfloodlight.openflow.protocol.OFPacketOut;
-import org.projectfloodlight.openflow.protocol.OFPortDesc;
+import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFType;
-import org.projectfloodlight.openflow.protocol.action.OFAction;
-import org.projectfloodlight.openflow.protocol.action.OFActionOutput;
-import org.projectfloodlight.openflow.protocol.action.OFActionOutput.Builder;
-import org.projectfloodlight.openflow.protocol.action.OFActionSetField;
-import org.projectfloodlight.openflow.protocol.action.OFActions;
-import org.projectfloodlight.openflow.protocol.match.Match;
-import org.projectfloodlight.openflow.protocol.match.MatchField;
-import org.projectfloodlight.openflow.protocol.match.MatchFields;
-import org.projectfloodlight.openflow.protocol.oxm.OFOxm;
-import org.projectfloodlight.openflow.protocol.oxm.OFOxms;
-import org.projectfloodlight.openflow.protocol.ver13.OFActionsVer13;
 import org.projectfloodlight.openflow.types.DatapathId;
-import org.projectfloodlight.openflow.types.EthType;
-import org.projectfloodlight.openflow.types.ICMPv4Type;
-import org.projectfloodlight.openflow.types.IPv4Address;
-import org.projectfloodlight.openflow.types.IpProtocol;
-import org.projectfloodlight.openflow.types.OFBufferId;
-import org.projectfloodlight.openflow.types.OFPort;
-import org.projectfloodlight.openflow.types.TableId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -69,16 +34,18 @@ import org.slf4j.LoggerFactory;
  * @author felipe.nesello
  *
  */
-public class GreenNetworkController implements
-		IFloodlightModule, IOFMessageListener, ILinkDiscoveryListener, IOFSwitchListener, ITopologyListener {
-	
-	protected IOFSwitchService swService;
-	protected ITopologyService topoService;
-	protected ILinkDiscoveryService linkDiscoverer;
-	protected IFloodlightProviderService floodlightProvider;
-	private TopologyManager topoManager;
-	protected static Logger logger;
+public class GreenNetworkController implements IFloodlightModule, IOFMessageListener, ITopologyListener {
 
+	protected static Logger logger = LoggerFactory.getLogger(GreenNetworkController.class);
+	
+	protected IFloodlightProviderService floodlightProvider;
+	protected IOFSwitchService switchService;
+	protected ITopologyService topologyService;
+	protected IRoutingService routingService;
+	protected IDeviceService deviceService;
+	
+	private final GNCPacketInProcessor packetProcessor = new GNCPacketInProcessor(this);
+	
 	@Override
 	public String getName() {
 		return GreenNetworkController.class.getSimpleName();
@@ -86,13 +53,11 @@ public class GreenNetworkController implements
 
 	@Override
 	public boolean isCallbackOrderingPrereq(OFType type, String name) {
-		// TODO Auto-generated method stub
-		return false;
+		return (type.equals(OFType.PACKET_IN) && (name.equals("topology") || name.equals("devicemanager")));
 	}
 
 	@Override
 	public boolean isCallbackOrderingPostreq(OFType type, String name) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -102,130 +67,15 @@ public class GreenNetworkController implements
 		
 		switch (msg.getType()) {
 		case PACKET_IN:
-			Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-			logger.info("Enabled ports of {}: {}", sw.getId().toString(), sw.getEnabledPortNumbers().toString());
-
-			if (eth.getEtherType() == EthType.IPv4) {
-				IPv4 ip = (IPv4) eth.getPayload();
-				
-//				processOutputPacket(sw.getOFFactory(), eth);
-//				buildFlowOnSwitch(sw);
-
-				if (ip.getProtocol().equals(IpProtocol.ICMP)) {
-					ICMP icmp = (ICMP) ip.getPayload();
-
-					if (icmp.getIcmpType() == ICMP.ECHO_REQUEST) {
-						logger.info("Ping detected: ICMP Request from host {} to {} in switch {}",
-								ip.getSourceAddress().toString(), ip.getDestinationAddress().toString(), sw.getId().toString());
-					} else if (icmp.getIcmpType() == ICMP.ECHO_REPLY) {
-						logger.info("Ping detected: ICMP Reply from host {} to {} in switch {}",
-								ip.getSourceAddress().toString(), ip.getDestinationAddress().toString(), sw.getId().toString());
-					}
-				}
+			logger.info("Got a packet in message from switch {}.", sw.getId().toString());
+			
+			if (packetProcessor.processPacket(sw, cntx, (OFPacketIn) msg)) {
+				logger.info("Packet processed successfully!");
 			}
-			break;
 		default:
 			break;
 		}
-		return Command.STOP;
-	}
-	
-	private void printSwitches() {
-		Set<DatapathId> dpIds = new HashSet<DatapathId>();
-		
-		dpIds = swService.getAllSwitchDpids();
-		logger.info("{} switches found:", dpIds.size());
-		for (DatapathId datapathId : dpIds) {
-			logger.info(datapathId.toString());
-		}
-	}
-	
-	private void printLinks() {
-		Map<Link, LinkInfo> links = linkDiscoverer.getLinks();
-		logger.info("{} links found: {}", links.size(), links.toString());
-	}
-
-	private void removeSwitch() {
-		IOFSwitch sw1 = swService.getSwitch(DatapathId.of("00:00:00:00:00:00:00:01"));
-		if (sw1 != null) {
-			logger.info("Removing switch {}...", sw1.getId().toString());
-			
-			topoManager.removeSwitch(sw1.getId());
-			logger.info("Switch {} removed sucessfully.", sw1.getId().toString());
-		}
-	}
-
-	private void buildFlowOnSwitch(IOFSwitch sw) {
-		OFFactory factory = sw.getOFFactory();
-		Match match = factory.buildMatch()
-				.setExact(MatchField.IN_PORT, OFPort.of(1))
-				.setExact(MatchField.ETH_TYPE, EthType.IPv4)
-				.setExact(MatchField.IPV4_SRC, IPv4Address.of("10.0.0.1"))
-				.setExact(MatchField.IP_PROTO, IpProtocol.ICMP)
-				.setExact(MatchField.ICMPV4_TYPE, ICMPv4Type.ECHO)
-				.build();
-		
-		ArrayList<OFAction> actionList = new ArrayList<OFAction>();
-		OFActions actions = factory.actions();
-//		OFOxms oxms = factory.oxms();
-//		
-//		OFActionSetField setDlDst = actions.buildSetField()
-//			    .setField(
-//			    		oxms.buildIpv4Dst()
-//			    		.setValue(IPv4Address.of("10.0.0.1"))
-//			    		.build()
-//			    		)
-//			        .build();
-		
-		OFActionOutput output = actions.buildOutput()
-				.setPort(OFPort.of(2))
-				.build();
-		actionList.add(output);
-		
-		OFFlowAdd flowAdd = factory.buildFlowAdd()
-			    .setBufferId(OFBufferId.NO_BUFFER)
-			    .setHardTimeout(3600)
-			    .setIdleTimeout(60)
-			    .setPriority(32768)
-			    .setMatch(match)
-			    .setActions(actionList)
-			    .setTableId(TableId.of(0))
-			    .setOutPort(OFPort.of(2))
-			    .build();
-		
-		sw.write(flowAdd);
-	}
-
-	private void processOutputPacket(IOFSwitch sw, Ethernet ethPacket) {
-		
-		if (sw.getId().equals(DatapathId.of("00:00:00:00:00:00:00:02"))) {
-
-			//		OFActions actions = new OFActionsVer13();
-			// Builder builder = actions.buildOutput();
-			/* Specify the switch port(s) which the packet should be sent out. */
-			OFActionOutput output = sw.getOFFactory().actions().buildOutput()
-					.setPort(OFPort.of(2))
-					.build();
-
-			/* 
-			 * Compose the OFPacketOut with the above Ethernet packet as the 
-			 * payload/data, and the specified output port(s) as actions.
-			 */
-			OFPacketOut myPacketOut = sw.getOFFactory().buildPacketOut()
-					.setData(ethPacket.serialize())
-					.setBufferId(OFBufferId.NO_BUFFER)
-					.setActions(Collections.singletonList((OFAction) output))
-					.build();
-
-			/* Write the packet to the switch via an IOFSwitch instance. */
-			sw.write(myPacketOut);
-		}
-
-		//		OFPacketOut po = sw.getOFFactory().buildPacketOut()
-		//			    .setActions(Collections.singletonList((OFAction) sw.getOFFactory().actions().output(OFPort.FLOOD, 0xffFFffFF)))
-		//			    .setInPort(OFPort.CONTROLLER)
-		//			    .build();
-		//		sw.write(po);
+		return Command.CONTINUE;
 	}
 
 	@Override
@@ -244,9 +94,10 @@ public class GreenNetworkController implements
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
 		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
 		l.add(IFloodlightProviderService.class);
-		l.add(ILinkDiscoveryService.class);
 		l.add(ITopologyService.class);
 		l.add(IOFSwitchService.class);
+		l.add(IRoutingService.class);
+		l.add(IDeviceService.class);
 		return l;
 	}
 
@@ -254,10 +105,10 @@ public class GreenNetworkController implements
 	public void init(FloodlightModuleContext context)
 			throws FloodlightModuleException {
 		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
-		linkDiscoverer = context.getServiceImpl(ILinkDiscoveryService.class);
-		topoService = context.getServiceImpl(ITopologyService.class);
-		swService = context.getServiceImpl(IOFSwitchService.class);
-		topoManager = context.getServiceImpl(TopologyManager.class);
+		topologyService = context.getServiceImpl(ITopologyService.class);
+		switchService = context.getServiceImpl(IOFSwitchService.class);
+		routingService = context.getServiceImpl(IRoutingService.class);
+		deviceService = context.getServiceImpl(IDeviceService.class);
 	    logger = LoggerFactory.getLogger(GreenNetworkController.class);
     }
 
@@ -265,55 +116,27 @@ public class GreenNetworkController implements
 	public void startUp(FloodlightModuleContext context)
 			throws FloodlightModuleException {
 		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
-		linkDiscoverer.addListener(this);
-		topoService.addListener(this);
-	}
-
-	@Override
-	public void switchAdded(DatapathId switchId) {
-		logger.info("Switch {} added to the network.", switchId.toString());
-	}
-
-	@Override
-	public void switchRemoved(DatapathId switchId) {
-		logger.info("Switch {} removed from the network.", switchId.toString());
-	}
-
-	@Override
-	public void switchActivated(DatapathId switchId) {
-		logger.info("Switch {} activated on the network.", switchId.toString());
-	}
-
-	@Override
-	public void switchPortChanged(DatapathId switchId, OFPortDesc port,
-			PortChangeType type) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void switchChanged(DatapathId switchId) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void linkDiscoveryUpdate(LDUpdate update) {
-		logger.info("Link updated.");
-	}
-
-	@Override
-	public void linkDiscoveryUpdate(List<LDUpdate> updateList) {
-		logger.info("Links updated.");
+		topologyService.addListener(this);
 	}
 
 	@Override
 	public void topologyChanged(List<LDUpdate> linkUpdates) {
 		logger.info("Topology updated.");
-		
 		printSwitches();
-		printLinks();
-		removeSwitch();
+	}
+	
+	private void printSwitches() {
+		Set<DatapathId> dpIds = new HashSet<DatapathId>();
+		
+		dpIds = switchService.getAllSwitchDpids();
+		StringBuilder output = new StringBuilder(String.valueOf(dpIds.size()));
+		
+		output.append(" switches found:\n");
+		for (DatapathId datapathId : dpIds) {
+			output.append(datapathId.toString());
+			output.append("\n");
+		}
+		logger.info(output.toString());
 	}
 
 }
